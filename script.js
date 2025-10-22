dfc3d5855217f2a60a73c9d5c0abb4cf9b545395
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-10-22 11:20:24 -0400

    chore: add pseudo-code for displaying books

--- script.js
+++ script.js
@@ -18,3 +18,10 @@ function addBookToLibrary(title, author, pages, status) {
 // addBookToLibrary("Glorious Rivals", "Jennifer Lynn Barnes", 64, "reading");
 
 // console.log(libraryOfBooks);
+
+// pseudocode!!
+// we can have a grid container in the html
+// for every book obj in the array...
+// create an element populated with the book obj's info (make sure it has a class attribute)
+// add this element to the DOM, appending it as a child node of the grid container
+// in the css, target the element's class attribute to style it as a grid item in the grid container
