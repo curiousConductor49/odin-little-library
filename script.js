56764d7962d23eb30870f251c0730978f0bddfdc
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-10-24 18:01:50 -0400

    feat: call function to add book to library when dialog is closed and form is submitted

--- script.js
+++ script.js
@@ -71,25 +71,31 @@ function createNewBookFromUser(event) {
     const pagesInput = document.getElementById("book-page-count");
     const statusInput = document.getElementById("book-status");
 
-    const formInputString = [...authorInput.value, titleInput.value, pagesInput.value, statusInput.value];
+    const formInputString = [titleInput.value, authorInput.value,pagesInput.value, statusInput.value].toString();
 
     event.preventDefault();
     newBookFormContainer.close(formInputString);
-    console.log(formInputString);
 }
 
 newBookBtn.addEventListener("click", () => {newBookFormContainer.showModal()});
 submitBtn.addEventListener("click", createNewBookFromUser);
+newBookFormContainer.addEventListener("close", (e) => {
+    const inputArray = newBookFormContainer.returnValue.split(",");
+    console.log(inputArray);
+    addBookToLibrary(inputArray[0], inputArray[1], parseInt(inputArray[2]), inputArray[3]);
+    // console.log(libraryOfBooks);
+})
 
 // test calls and logs
 // addBookToLibrary("TWSA", "tls123", 3149, "read");
 // addBookToLibrary("Glorious Rivals", "Jennifer Lynn Barnes", 64, "reading");
-
-console.log(libraryOfBooks);
+// console.log(libraryOfBooks);
 displayBooksInLibrary(libraryOfBooks);
 
 // pseudocode!!
 // create an html form (text inputs for author, title, status; number input for page count; button to submit) inside a dialog, and save their DOM nodes to variables for later access
 // set the dialog to be opened by a button once clicked (aka showModal() inside the event listener's function)
 // set the dialog to be closed when the form button is clicked; use event.preventDefault() to prevent form submission, and "manually" close the dialog via close()
-// either way, we can use the value property for each form input to get what the user inputted—pass these values to the add book function, which should be called when user submits the form aka clicks the submit button regardless of how the dialog is closed
+// ISSUE: need to figure out how to update displayBooksInLibrary() after the form is submitted, for some reason neither it nor library are updated
+// ISSUE: form controls aren't cleared each time the dialog is opened, need to reset them each time
+// might want to add a button to cancel the dialog if no book is intended to be added + make sure blank inputs aren't allowed
